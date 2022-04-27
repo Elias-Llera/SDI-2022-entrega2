d73c4e32a1b9dab1177639813c8ce9270155b6b1
Elias <eliaslleragr@gmail.com>
2022-04-27 18:46:45 +0200

    Funciones base de los repositorios de posts y frienships

--- sdi-entrega2-41/repositories/friendshipsRepository.js
+++ sdi-entrega2-41/repositories/friendshipsRepository.js
@@ -6,6 +6,71 @@ module.exports = {
     init: function (app, mongoClient) {
         this.mongoClient = mongoClient;
         this.app = app;
-    }
+    },
+
+    getFriendships: async function (filter, options) {
+        try {
+            const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
+            const database = client.db("social");
+            const collectionName = 'friendships';
+            const friendshipsCollection = database.collection(collectionName);
+            return await friendshipsCollection.find(filter, options).toArray();
+        } catch (error) {
+            throw (error);
+        }
+    },
+
+    getFriendshipsPg: async function (filter, options, page) {
+        try {
+            const limit = 4;
+            const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
+            const database = client.db("social");
+            const collectionName = 'friendships';
+            const friendshipsCollection = database.collection(collectionName);
+            const friendshipsCollectionCount = await friendshipsCollection.count();
+            const cursor = friendshipsCollection.find(filter, options).skip((page - 1) * limit).limit(limit)
+            const friendships = await cursor.toArray();
+            return {friendships: friendships, total: friendshipsCollectionCount};
+        } catch (error) {
+            throw (error);
+        }
+    },
+
+    findFriendship: async function (filter, options) {
+        try {
+            const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
+            const database = client.db("social");
+            const collectionName = 'friendships';
+            const friendshipsCollection = database.collection(collectionName);
+            return await friendshipsCollection.findOne(filter, options);
+        } catch (error) {
+            throw (error);
+        }
+    },
+
+    insertFriendship: async function (friendship) {
+        try{
+            const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
+            const database = client.db("social");
+            const collectionName = 'friendships';
+            const friendshipsCollection = database.collection(collectionName);
+            const result = await friendshipsCollection.insertOne(friendship);
+            return result.insertedId;
+        } catch(error) {
+            throw (error);
+        }
+    },
+
+    deleteFriendship: async function (filter, options) {
+        try {
+            const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
+            const database = client.db("social");
+            const collectionName = 'friendships';
+            const friendshipsCollection = database.collection(collectionName);
+            return await friendshipsCollection.deleteOne(filter, options);
+        } catch (error) {
+            throw (error);
+        }
+    },
 
 };
--- sdi-entrega2-41/repositories/postsRepository.js
+++ sdi-entrega2-41/repositories/postsRepository.js
@@ -6,6 +6,71 @@ module.exports = {
     init: function (app, mongoClient) {
         this.mongoClient = mongoClient;
         this.app = app;
-    }
+    },
+
+    getPosts: async function (filter, options) {
+        try {
+            const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
+            const database = client.db("social");
+            const collectionName = 'posts';
+            const postsCollection = database.collection(collectionName);
+            return await postsCollection.find(filter, options).toArray();
+        } catch (error) {
+            throw (error);
+        }
+    },
+
+    getPostsPg: async function (filter, options, page) {
+        try {
+            const limit = 4;
+            const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
+            const database = client.db("social");
+            const collectionName = 'posts';
+            const postsCollection = database.collection(collectionName);
+            const postsCollectionCount = await postsCollection.count();
+            const cursor = postsCollection.find(filter, options).skip((page - 1) * limit).limit(limit)
+            const posts = await cursor.toArray();
+            return {posts: posts, total: postsCollectionCount};
+        } catch (error) {
+            throw (error);
+        }
+    },
+
+    findPost: async function (filter, options) {
+        try {
+            const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
+            const database = client.db("social");
+            const collectionName = 'posts';
+            const postsCollection = database.collection(collectionName);
+            return await postsCollection.findOne(filter, options);
+        } catch (error) {
+            throw (error);
+        }
+    },
+
+    insertPost: async function (post) {
+        try{
+            const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
+            const database = client.db("social");
+            const collectionName = 'posts';
+            const postsCollection = database.collection(collectionName);
+            const result = await postsCollection.insertOne(post);
+            return result.insertedId;
+        } catch(error) {
+            throw (error);
+        }
+    },
+
+    deletePost: async function (filter, options) {
+        try {
+            const client = await this.mongoClient.connect(this.app.get('connectionStrings'));
+            const database = client.db("social");
+            const collectionName = 'posts';
+            const postsCollection = database.collection(collectionName);
+            return await postsCollection.deleteOne(filter, options);
+        } catch (error) {
+            throw (error);
+        }
+    },
 
 };
