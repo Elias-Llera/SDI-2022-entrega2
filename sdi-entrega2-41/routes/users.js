b52feecafe035138d92948de58bcde90a315e869
uo265488 <uo265488@uniovi.es>
2022-05-08 20:34:16 +0200

    Update users.js

--- sdi-entrega2-41/routes/users.js
+++ sdi-entrega2-41/routes/users.js
@@ -2,8 +2,8 @@ const {ObjectId} = require("mongodb");
 
 module.exports = function (app, usersRepository) {
 
-    app.get("/users", function (req, res) {
-        //let filter = u=>{return u.rol !== "ADMIN" && u._id !== ObjectId(req.session.user)};
+    app.get("/users/list", function (req, res) {
+        let filter = { rol : { $not: { $eq: "ADMIN"}}, email: { $not: { $eq : req.session.user }}};
         let options = {};
 
         //For pagination
@@ -13,63 +13,60 @@ module.exports = function (app, usersRepository) {
             page = 1;
         }
 
-        console.log(req.session.user);
-        usersRepository.findUser({ email : req.session.user }, options).then(result => {
-            console.log(result)
-            usersRepository.getUsersPg({ }, options, page)
-                .then( result => {
-                    console.log(result)
-                    // Cálculos de paginación
-                    let lastPage = result.total / 5;
-                    if (result.total % 5 > 0) { // Decimales
-                        lastPage = lastPage + 1;
+        usersRepository.getUsersPg(filter, options, page)
+            .then(result => {
+                // Cálculos de paginación
+                let lastPage = result.total / 5;
+                if (result.total % 5 > 0) { // Decimales
+                    lastPage = lastPage + 1;
+                }
+                let pages = []; // Indices de páginas a mostrar
+                for (let i = page - 2; i <= page + 2; i++) {
+                    if (i > 0 && i <= lastPage) {
+                        pages.push(i);
                     }
-                    let pages = []; // Indices de páginas a mostrar
-                    for (let i = page - 2; i <= page + 2; i++) {
-                        if (i > 0 && i <= lastPage) {
-                            pages.push(i);
-                        }
-                    }
-                    let response = {
-                        users: result.posts,
-                        pages: pages,
-                        currentPage: page
-                    }
-                    res.render("users/list.twig", response);
-                })
-                .catch( error =>
-                    res.send("Error: " + error)
-                );
-        })
+                }
+                let response = {
+                    users: result.users,
+                    pages: pages,
+                    currentPage: page,
+                    session: req.session
+                }
+                res.render("users/list.twig", response);
+            })
+            .catch(error =>
+                res.send("Error: " + error)
+            );
+    })
 
-    });
+    ;
 
     app.get('/users/login', function (req, res) {
         res.render("login.twig");
     });
 
-    app.post('/users/login',function (req,res) {
-        let securePassword = app.get("crypto").createHmac('sha256',app.get('clave'))
+    app.post('/users/login', function (req, res) {
+        let securePassword = app.get("crypto").createHmac('sha256', app.get('clave'))
             .update(req.body.password).digest('hex');
         let filter = {
             email: req.body.email,
             password: securePassword
         }
         let options = {}
-        usersRepository.findUser(filter,options).then(user => {
-            if(user == null){
+        usersRepository.findUser(filter, options).then(user => {
+            if (user == null) {
                 req.session.user = null;
                 res.redirect("/users/login" +
-                    "?message=Email o password incorrecto"+
+                    "?message=Email o password incorrecto" +
                     "&messageType=alert-danger ");
-            }else{
+            } else {
                 req.session.user = user.email;
-                res.redirect("/users");
+                res.redirect("/users/list");
             }
         }).catch(error => {
             req.session.user = null;
             res.redirect("/users/login" +
-                "?message=Se ha producido un error al buscar el usuario"+
+                "?message=Se ha producido un error al buscar el usuario" +
                 "&messageType=alert-danger ");
         })
     });
@@ -84,14 +81,15 @@ module.exports = function (app, usersRepository) {
             .update(req.body.password).digest('hex');
         let user = {
             email: req.body.email,
-            password: securePassword
+            password: securePassword,
+            rol: "STANDARD"
         }
-        await validateUser(user).then(result =>{
+        await validateUser(user).then(result => {
 
 
-            if(result.length > 0) {
+            if (result.length > 0) {
                 let url = ""
-                for(error in result){
+                for (error in result) {
                     url += "&message=" + result[error] + "&messageType=alert-danger "
                 }
                 res.redirect("/users/signup?" + url);
@@ -105,29 +103,28 @@ module.exports = function (app, usersRepository) {
         });
     })
 
-     async function validateUser(user){
+    async function validateUser(user) {
         let errors = [];
-        if(user.email == null || user.email == ""){
+        if (user.email == null || user.email == "") {
             errors.push("El email es obligatorio");
         }
-        if(user.password == null || user.password == ""){
+        if (user.password == null || user.password == "") {
             errors.push("El password es obligatorio");
         }
         //check that the email format is correct
         let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if(!emailRegex.test(user.email)){
+        if (!emailRegex.test(user.email)) {
             errors.push("El email no tiene un formato correcto");
         }
         let userfound = await usersRepository.findUser({email: user.email});
 
-            if(userfound != null){
-                errors.push("El email ya existe");
-            }
-            return errors;
+        if (userfound != null) {
+            errors.push("El email ya existe");
+        }
+        return errors;
 
 
     }
-
 
 
 }
