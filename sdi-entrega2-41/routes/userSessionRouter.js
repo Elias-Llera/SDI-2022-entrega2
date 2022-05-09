1af8d1a578cec0c65b9d494df29cd72bcc89214f
uo265488 <uo265488@uniovi.es>
2022-05-09 12:00:17 +0200

    Create userSessionRouter.js

---
+++ sdi-entrega2-41/routes/userSessionRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const userSessionRouter = express.Router();
+userSessionRouter.use(function(req, res, next) {
+    console.log("routerUsuarioSession");
+    if ( req.session.user ) {
+        // dejamos correr la petición
+        next();
+    } else {
+        res.redirect("/users/login");
+    }
+});
+module.exports = userSessionRouter;
