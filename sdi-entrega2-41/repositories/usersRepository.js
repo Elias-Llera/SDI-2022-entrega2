13623de6698a7e53bd356fcd16ebda4eaf8f7fd7
uo265488 <uo265488@uniovi.es>
2022-05-09 11:40:00 +0200

    Update usersRepository.js

--- sdi-entrega2-41/repositories/usersRepository.js
+++ sdi-entrega2-41/repositories/usersRepository.js
@@ -28,10 +28,10 @@ module.exports = {
             const database = client.db("entrega2");
             const collectionName = 'users';
             const usersCollection = database.collection(collectionName);
-            const usersCollectionCount = await usersCollection.count();
+            const usersCollectionCount = await usersCollection.find(filter, options).count();
             const cursor = usersCollection.find(filter, options).skip((page - 1) * limit).limit(limit)
             const users = await cursor.toArray();
-            return {users: users, total: usersCollectionCount};
+            return {users: users, total: usersCollectionCount };
         } catch (error) {
             throw (error);
         }
